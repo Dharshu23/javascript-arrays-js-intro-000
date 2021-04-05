f1e91ddf0dd595e7d06a782aa8713902ec6de81c
Priyadharshini Thirukonda Mohanlal <priyamohanlal23@gmail.com>
2021-04-05 21:38:22 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,18 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(myArray, myElement){
+ ["Dairy Milk", ...addElementToBeginningOfArray];
+  return chocolateBars;
+}
+
+function destructivelyAddElementToBeginningOfArray(myArray, myElement){
+
+}
+
+function addElementToEndOfArray(myArray, myElement){
+
+}
+
+function destructivelyAddElementToEndOfArray(myArray, myElement){
+
+}
